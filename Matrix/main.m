adaaabfa0c515e94787295cb0308a3c1eff984a8
Ryan Poolos <ryanpoolos@Gmail.com>
2013-08-23 21:45:29 -0400

    Initial Commit

---
+++ Matrix/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matrix
+//
+//  Created by Ryan Poolos on 8/23/13.
+//  Copyright (c) 2013 Cocoaheads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
