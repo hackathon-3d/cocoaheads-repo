3cc5464be40bd5302177f57c2717da73fbbaa31a
Ryan Poolos <ryanpoolos@Gmail.com>
2013-08-23 21:47:28 -0400

    Initial Commit.

---
+++ Matrix/GridViewController.h
@@ -0,0 +1,13 @@
+//
+//  GridViewController.h
+//  Matrix
+//
+//  Created by Ryan Poolos on 8/23/13.
+//  Copyright (c) 2013 Cocoaheads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GridViewController : UIViewController
+
+@end
---
+++ Matrix/GridViewController.m
@@ -0,0 +1,38 @@
+//
+//  GridViewController.m
+//  Matrix
+//
+//  Created by Ryan Poolos on 8/23/13.
+//  Copyright (c) 2013 Cocoaheads. All rights reserved.
+//
+
+#import "GridViewController.h"
+
+@interface GridViewController ()
+
+@end
+
+@implementation GridViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
